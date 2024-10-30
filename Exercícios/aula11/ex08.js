2328f7592d352015f7d8e7d36f5bdd4d9760141b
gabriellaxavier05 <gabriellaxaviersp05@outlook.com>
2024-10-30 01:04:58 -0300

    Exercícios da aula 10 - Condições (Parte 1)

---
+++ Exercícios/aula11/ex08.js
@@ -0,0 +1,19 @@
+/* Para escrever no console
+console.log('O console funcionou corretamente!')
+*/
+
+/* Ex. 2
+var velocidade = 60.5
+console.log(`A velocidade do seu carro é ${velocidade} Km/h`)
+console.log('Dirija sempre usando seu cinto de segurança!')
+*/
+
+// Os comandos acima 👆🏼 são SEQUENCIAIS.
+
+// Trabalhando com if-else | Condição simples
+var velocidade = 60.5
+console.log(`A velocidade do seu carro é ${velocidade} Km/h`)
+if (velocidade > 60){ // Se a condição for verdadeira
+    console.log('Você está ultrapassando a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando seu cinto de segurança!')
---
+++ Exercícios/aula11/ex09.js
@@ -0,0 +1,9 @@
+// Condicional composta
+var pais = 'Suécia'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') { // se a condição for verdadeira
+    console.log('Você é brasileiro(a)!')
+}
+else { // se a condição for falsa
+    console.log('Você é estrangeiro(a)!')
+}
