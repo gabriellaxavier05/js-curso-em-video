ee90fc26f3276594272f37ddb02f59cd13d868cc
gabriellaxavier05 <gabriellaxaviersp05@outlook.com>
2024-12-24 02:53:52 -0300

    Exercícios da aula 13 do mód Retições em JS

---
+++ Exercícios/aula13/ambiente_do_while.js
@@ -0,0 +1,9 @@
+// Executando o texto "Tudo bem?" durante 6x usando o laço do while
+var c = 1 // contador
+
+do {
+    console.log('Tudo bem?')
+    console.log('Vez: ' + c)
+    console.log('-----------')
+    c = c + 1
+} while (c <= 6)
---
+++ Exercícios/aula13/ambiente_while.js
@@ -0,0 +1,9 @@
+// Executando o texto "Tudo bem?" durante 6x usando o laço while
+var c = 1 // contador
+
+while (c <= 6){
+    console.log('Tudo bem?')
+    console.log('Vez: ' + c)
+    console.log('-----------')
+    c = c + 1
+}
